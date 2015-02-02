6cb151878348526fb05072ba07c7d921e6cc6c50
Rob Squires <rob.squires.ext@bbc.co.uk>
2015-02-02 14:38:49 +0000

    moved to revealing module pattern

--- app/scripts/model/ticker.js
+++ app/scripts/model/ticker.js
@@ -5,69 +5,81 @@ define([
 
     var Ticker = function(id, symbol, price, quantity) {
 
-      this.id = id;
-      this.symbol = symbol;
+      var bid = null,
+          offer = null,
+          change = null,
+          changePct = null,
+          quoteChange = null,
+          quoteChangePct = null,
+          owns = null,
+          marketPrice = null;
 
-      this.setPrice(price);
-      this.setQuantity(quantity);
+      function updateQuote(quote) {
+        bid = parseFloat(quote.BidRealtime);
+        offer = parseFloat(quote.AskRealtime);
+        quoteChange = parseFloat(quote.ChangeRealtime);
+        quoteChangePct = quote.ChangeinPercent;
 
-
-      this.bid = null;
-      this.offer = null;
-      this.change = null;
-      this.changePct = null;
-      this.quoteChange = null;
-      this.quoteChangePct = null;
-
-      Object.defineProperty(this, 'owns', {
-        get: function() {
-          return this.quantity !== null;
-        }
-      });
-
-      //computed values
-      Object.defineProperty(this, 'marketPrice', {
-        get: function() {
-          return this.owns ? this.bid : this.offer;
-        }
-      });
-    };
-
-    Ticker.prototype = {
-
-      updateQuote: function(quote) {
-        this.bid = parseFloat(quote.BidRealtime);
-        this.offer = parseFloat(quote.AskRealtime);
-        this.quoteChange = parseFloat(quote.ChangeRealtime);
-        this.quoteChangePct = quote.ChangeinPercent;
-
-        refresh.call(this);
-      },
-
-      movement: function() {
-        return this.change < 0 ? -1 : 1;
-      },
-
-      setQuantity: function(quantity) {
-        this.quantity = quantity !== null ? parseInt(quantity, 10) : null;
-        refresh.call(this);
-      },
-
-      setPrice: function(price) {
-        this.price = price !== null ? parseFloat(price) : null;
-        refresh.call(this);
+        refresh();
       }
-    };
 
-
-    var refresh = function() {
-      if (this.owns) {
-        this.change = parseFloat(this.offer - this.price);
-        this.changePct = (this.change / this.price * 100).toFixed(2).toString() + '%';
-      } else {
-        this.change = this.quoteChange;
-        this.changePct = this.quoteChangePct;
+      function movement() {
+        return change < 0 ? -1 : 1;
       }
+
+      function setQuantity(newQuantity) {
+        quantity = newQuantity !== null ? parseInt(newQuantity, 10) : null;
+        owns = quantity !== null;
+        refresh();
+      }
+
+      function setPrice(newPrice) {
+        price = newPrice !== null ? parseFloat(newPrice) : null;
+        refresh();
+      }
+
+      /**
+       * private functions
+       */
+      function refresh() {
+
+        if (owns) {
+          marketPrice = bid;
+          change = parseFloat(offer - price);
+          changePct = (change / price * 100).toFixed(2).toString() + '%';
+        } else {
+          marketPrice = offer;
+          change = quoteChange;
+          changePct = quoteChangePct;
+        }
+      }
+
+      setPrice(price);
+      setQuantity(quantity);
+      
+      return {
+        /**
+         * public props
+         */
+        id: id,
+        symbol: symbol,
+        price: price,
+        quantity: quantity,
+        
+        change: change,
+        changePct: changePct,
+        quoteChange: quoteChange,
+        quoteChangePct: quoteChangePct,
+
+        /**
+         * public methods
+         */
+        updateQuote: updateQuote,
+        movement: movement,
+        setQuantity: setQuantity,
+        setPrice: setPrice
+      };
     };
+
     return Ticker;
   });
